951c5495edcf4fdd20f2fb03ad6915391d4b765c
Mikhail Potapov <devslashnil@gmail.com>
2022-09-07 16:30:01 +0300

    added 4 half

--- pattern/04_command.go
+++ pattern/04_command.go
@@ -19,3 +19,62 @@ package pattern
 	Реальные примеры:
 	-
 */
+
+// Command интерфейс команды
+type Command interface {
+	Execute()
+}
+
+// Command1 реализует интерфейс Command
+type Command1 struct {
+	receiver *Receiver
+}
+
+// Execute выполняет команду
+func (c *Command1) Execute() {
+	c.receiver.Action1()
+}
+
+// Command2 реализует интерфейс Command
+type Command2 struct {
+	receiver *Receiver
+}
+
+// Execute выполняет команду
+func (c *Command2) Execute() {
+	c.receiver.Action2()
+}
+
+// Receiver реализация ресивера
+type Receiver struct {
+}
+
+// Action1 действие Receiver
+func (r *Receiver) Action1() {}
+
+// Action2 действие Receiver
+func (r *Receiver) Action2() {}
+
+// Invoker реализация инвокера
+type Invoker struct {
+	commands []Command
+}
+
+// StoreCommand добавляет команду в инвокер
+func (i *Invoker) StoreCommand(command Command) {
+	i.commands = append(i.commands, command)
+}
+
+// UnStoreCommand убирает команду в инвокере
+func (i *Invoker) UnStoreCommand() {
+	if len(i.commands) != 0 {
+		i.commands = i.commands[:len(i.commands)-1]
+	}
+}
+
+// Execute выполняет все команды инвокера
+func (i *Invoker) Execute() {
+	for _, command := range i.commands {
+		command.Execute()
+	}
+}
