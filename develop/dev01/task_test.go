1b11cf39f20dc6b566ced27f943086cd789534cd
Mikhail Potapov <devslashnil@gmail.com>
2022-09-08 18:38:36 +0300

    added 2nd develop

--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+// TestDev01 проверяет формат и часовой пояс GetTime()
+func TestDev01(t *testing.T) {
+	got := GetTime()
+	want := time.Now().UTC().Format(time.Kitchen)
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,70 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "Test",
+			input:   "a4bc2d5e",
+			want:    "aaaabccddddde",
+			wantErr: nil,
+		},
+		{
+			name:    "Test",
+			input:   "abcd",
+			want:    "abcd",
+			wantErr: nil,
+		},
+		{
+			name:    "Test",
+			input:   "45",
+			want:    "",
+			wantErr: inputErr,
+		},
+		{
+			name:    "Test",
+			input:   "",
+			want:    "",
+			wantErr: nil,
+		},
+		{
+			name:    "Test",
+			input:   `qwe\4\5`,
+			want:    `qwe45`,
+			wantErr: nil,
+		},
+		{
+			name:    "Test",
+			input:   `qwe\45`,
+			want:    `qwe44444`,
+			wantErr: nil,
+		},
+		{
+			name:    "Test",
+			input:   `qwe\\5`,
+			want:    `qwe\\\\\`,
+			wantErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := Unpack(test.input)
+			if err != test.wantErr {
+				t.Errorf("got %q want %q", err, test.wantErr)
+			}
+			if got != test.want {
+				t.Errorf("got %q want %q", got, test.want)
+			}
+		})
+	}
+
+}
